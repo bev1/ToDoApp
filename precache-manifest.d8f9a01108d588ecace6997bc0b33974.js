df26bd1979a3c4293c800cb14e702c77a069c612
bev1 <longer30001@gmail.com>
2019-09-15 15:25:58 +0300

    Updates

---
+++ precache-manifest.d8f9a01108d588ecace6997bc0b33974.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b6308e151f2926ffc6535cc571c017a",
+    "url": "/ToDoApp/index.html"
+  },
+  {
+    "revision": "12d177b0536dee6e4620",
+    "url": "/ToDoApp/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "cfee6d04c85bd9b7b13c",
+    "url": "/ToDoApp/static/js/2.109f4f13.chunk.js"
+  },
+  {
+    "revision": "12d177b0536dee6e4620",
+    "url": "/ToDoApp/static/js/main.0757fa93.chunk.js"
+  },
+  {
+    "revision": "b83bed652e482b610a59",
+    "url": "/ToDoApp/static/js/runtime~main.0fd72f62.js"
+  },
+  {
+    "revision": "1555d7ef8c10ec41529eb258c0471131",
+    "url": "/ToDoApp/static/media/nav_background.1555d7ef.jpg"
+  }
+]);
